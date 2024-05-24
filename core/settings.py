7a6c19223eee79e76e24a2707c247228c3f33979
kali <kali@kali.kali>
2024-05-24 03:46:08 -0500

    local changes

--- core/settings.py
+++ core/settings.py
@@ -188,11 +188,16 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
-STATIC_URL = 'static/'
+# Static files (CSS, JavaScript, Images)
+STATIC_URL = '/static/'
 
-# STATICFILES_DIRS = [
-#     os.path.join(BASE_DIR, 'static'),
-# ]
+# This is the directory where collectstatic will copy static files for deployment.
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+
+# This is the list of directories where Django will search for additional static files
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'static'),
+]
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
@@ -329,3 +334,4 @@ USER_LASTSEEN_TIMEOUT = 60 * 60 * 24 * 7
 
 # PAYSTACK_SECRET_KEY = os.environ.get('PAYSTACK_SECRET_KEY')
 # PAYSTACK_PUBLIC_KEY = env('PAYSTACK_PUBLIC_KEY')
+
