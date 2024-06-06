1c91f8bfd3f1d420a33ea96f023668f8ac7ac17c
kali <kali@kali.kali>
2024-06-06 09:30:51 -0500

    Added Authentication backend

--- users/utils.py
+++ users/utils.py
@@ -2,7 +2,7 @@ from django.conf import settings
 from django.core.mail import send_mail
 
 def send_activation_email(user):
-    activation_link = f"{settings.BASE_URL}/activate{user.activation_token}"
+    activation_link = f"{settings.BASE_URL}activate/{user.activation_token}"
     subject = "Activate your account"
     message = f"Hi {user.full_name},\n\nPlease click the following link to activate your account:\n{activation_link}"
     print(message)
