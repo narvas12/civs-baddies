500148dec76d7934bfc8d899e0a11d536a56153e
narvas12 <ebukaemmanuel71@gmail.com>
2024-09-28 09:24:33 -0500

    set user to be active by default

--- users/models.py
+++ users/models.py
@@ -37,7 +37,7 @@ class CustomUser(AbstractUser):
     updated_at = models.DateTimeField(auto_now=True)
     is_staff = models.BooleanField(default=False)
     is_superuser = models.BooleanField(default=False)
-    is_active = models.BooleanField(default=False)  
+    is_active = models.BooleanField(default=True)  
     activation_token = models.CharField(max_length=100, blank=True, null=True, editable=False) 
     USERNAME_FIELD = 'email'
     REQUIRED_FIELDS = ['username']
